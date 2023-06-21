d7a8a9446f607247fb7c7c049c348dce6396a799
Niapa <niapa7@yandex.ru>
2023-06-21 12:36:48 +0700

    3  commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+/*
 int InputNum(string message)
 {
     Console.Write(message);
@@ -11,16 +12,15 @@ int InputNum(string message)
 
 int Degree(int a, int b)
 {
-    int d = a;
     int s = 1;
-    if(s < b);
+    if (s < b) ;
     {
-        int compos = d * d;
+        int compos = a * a;
         s++;
     }
-        else 
+        else
     {
-        int compos = d * d;
+        compos = a * a;
     }
     return compos;
 }
@@ -29,15 +29,68 @@ int num = InputNum("Введите первое число A: ");
 int num1 = InputNum("Введите второе число B: ");
 int res = Degree(num, num1);
 Console.WriteLine($"Число A в степени B равно: {compos}");
-
+*/
 
 
 /*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 452 -> 11
 82 -> 10
 9012 -> 12
+*/
 
-Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/*int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int Count(int x)
+{
+    int sum = 0;
+    for(x>0)
+    {
+        int y = x%10;
+        int z = x/10;
+        int sum = y+sum;
+    }
+    return sum;
+}
+
+int num = InputNum("Введите целое число: ");
+int res = Count(num);
+Console.WriteLine($"Число A в степени B равно: {res}");
+
+*/
+
+
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+
+int[] ArrayRand(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(10);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i<array.Length-1)
+        Console.Write(", ");
+        else
+        Console.Write("]");
+    }
+}
+int size = 8;
+ArrayRand(size);
+Console.WriteLine("Массив из 8 элементов: ");
+int[] arr = ArrayRand(size);
+PrintArray(arr);
